fec202f0e308d5b58bb73474cdef3978726b9791
Nikita Jain <nikita.jain.ext@MVS-537.local>
2024-12-30 12:36:00 +0530

    Initial commit

---
+++ src/test/java/com/github/copilot/demo/copilot_demo/CopilotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.copilot.demo.copilot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
