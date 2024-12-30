15efd539bb02ed627cee37e153dea558e4176c4d
nikita.jain <nikitan.jain@infobeans.com>
2024-12-30 13:01:31 +0530

    Initial Configuration

--- src/test/java/com/github/copilot/demo/copilot_demo/CopilotDemoApplicationTests.java
+++ src/test/java/com/github/copilot/demo/copilot_demo/CopilotDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CopilotDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
